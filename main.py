60446b726363fec42be33298a1435556c4cab136
slipstream00 <cavinpabua28@gmail.com>
2023-04-19 13:08:52 +0800

    test

--- main.py
+++ main.py
@@ -290,13 +290,8 @@ def operate_dispenser(count, denomination):
                 # if GPIO.input(ir_sensor_4_pin):
                 if input_state_4 == GPIO.LOW:
                     dispenser_count += 1
-                    # if dispenser_count >= count:
-                        # GPIO.output(relay_4_pin, GPIO.LOW)
-                    prev_input_4 = input_state_4
-                    time.sleep(0.5)
-                else:
-                    prev_input_4 = input_state_4
-                    time.sleep(0.5)
+                prev_input_4 = input_state_4
+                time.sleep(0.5)
         # turn off relay 4
         if dispenser_count >= count:
             GPIO.output(relay_4_pin, GPIO.LOW)
